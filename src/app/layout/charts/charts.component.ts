5d538214d8e0606515c27c9d644c46ebf2bbfdbf
rohanmehto2 <rohanmehto1910@gmail.com>
2020-05-31 11:31:44 +0530

    again charts n stuff

--- src/app/layout/charts/charts.component.ts
+++ src/app/layout/charts/charts.component.ts
@@ -55,12 +55,8 @@ export class ChartsComponent implements OnInit {
     public polarAreaChartType: string;
 
     // lineChart
-    public lineChartData: Array<any> = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-        { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
-    ];
-    public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartData: Array<any> = [];
+    public lineChartLabels: Array<any> = [];
     public lineChartOptions: any = {
         responsive: true
     };
@@ -132,11 +128,18 @@ export class ChartsComponent implements OnInit {
         this.lineChartLegend = true;
         this.lineChartType = 'line';
 
-        const url = `/lcu`;
+        let url = `/lcu`;
         this.restService.get(url)
         .subscribe((data: any) => {
             this.pieChartLabels = data.data.names;
             this.pieChartData = data.data.powers;
+        });
+
+        url = '/lcutemp';
+        this.restService.get(url)
+        .subscribe((data: any) => {
+            this.lineChartData = data.data;
+            this.lineChartLabels = data.times;
         });
     }
 }
